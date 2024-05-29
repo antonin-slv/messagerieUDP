9b2685ac8e6f719219593b6169dc10805fdcd84f
Thomas Blanché <totodelaforet69@gmail.com>
2024-05-29 14:37:33 +0200

    add main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        if (args.length != 1) {
+            System.out.println("Wrong argument provided.");
+        } else {
+            if(args[0].equals("serv")){
+                System.out.println("Server mode");
+                Server server = new Server();
+                server.run();
+            }
+            else if(args[0].equals("cli")){
+                System.out.println("Client mode");
+                Client client = new Client();
+                client.run();
+            }
+            else{
+                System.out.println("Wrong argument provided.");
+            }
+        }
+    }
+}
