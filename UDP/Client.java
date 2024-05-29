7e287fd09247beaa2d45fbb96133b06182000496
Antonin Slv <antonin.sylvestre@orange.fr>
2024-05-29 15:37:47 +0200

    better Client

--- UDP/Client.java
+++ UDP/Client.java
@@ -1,7 +1,9 @@
 package UDP;
 
 import java.io.BufferedReader;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -11,8 +13,6 @@ import java.net.Socket;
 public class Client implements Runnable {
 
     public Client() {
-
-
         
     }
 
@@ -20,39 +20,49 @@ public class Client implements Runnable {
 
     @Override
     public void run() {
-        String message = "vide";
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        try{
-            message = reader.readLine();
-        }catch(IOException e){
-            e.printStackTrace();
-        }
 
 
-        byte[] data = message.getBytes();
+        int port = 12345;
+        DatagramSocket socket;
+        InetAddress addr;
         try {
-            InetAddress addr = InetAddress.getByName("localhost");
-            int port = 12345;
-            DatagramPacket packet = new DatagramPacket(data, data.length, addr, port);
-
-            DatagramSocket socket = new DatagramSocket();
-            socket.send(packet);
-
-
-
-            //le client attend la réponse
-            byte[] buffer = new byte[1024];
-            DatagramPacket response = new DatagramPacket(buffer, buffer.length);
-            socket.receive(response);
-            System.out.println("Client received : " + new String(response.getData()));
-
-            socket.close();
-        
+            addr = InetAddress.getByName("localhost");
+            socket = new DatagramSocket();
         } catch (IOException e) {
             e.printStackTrace();
+            return;
         }
+        String message;
+        while (true) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            try{
+                message = reader.readLine();
+            }catch(IOException e){
+                e.printStackTrace();
+                continue;
+            }
+            if (message.charAt(0) == '/') {
+                if (message.equals("/quit")) {
+                    break;
+                }
+            }
 
+            byte[] data = message.getBytes();
+            try {
 
+                DatagramPacket packet = new DatagramPacket(data, data.length, addr, port);
+                socket.send(packet);
+                //le client attend la réponse
+                byte[] buffer = new byte[1024];
+                DatagramPacket response = new DatagramPacket(buffer, buffer.length);
+                socket.receive(response);
+                System.out.println("Client received : " + new String(response.getData()));
+            
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        socket.close();
     }
     
 }
